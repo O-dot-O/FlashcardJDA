ec15b1c0df3e6e952b9aa68a8530d08473f0f0f4
Maxence Raynal <mraynal2008@gmail.com>
2020-01-25 21:46:36 +0100

    Navigation bar complete

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-  this.classList.toggle("active");
-  var dropdownContent = this.nextElementSibling;
-  if (dropdownContent.style.display === "block") {
-  dropdownContent.style.display = "none";
-  } else {
-  dropdownContent.style.display = "block";
-  }
+$(document).ready(function () {
+  $(".dropdown-btn").click(function () {
+    $(this).next().toggle();
+    $(this).next().toggleClass("active");
   });
-}
+});
