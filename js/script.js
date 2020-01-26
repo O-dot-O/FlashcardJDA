749dec5fe01e9ba843d696e59c9f93aff60fb890
Maxence Raynal <mraynal2008@gmail.com>
2020-01-26 12:02:49 +0100

    Side menu improvements

--- js/script.js
+++ js/script.js
@@ -2,5 +2,8 @@ $(document).ready(function () {
   $(".dropdown-btn").click(function () {
     $(this).next().toggle();
     $(this).next().toggleClass("active");
+    $(this).children().toggleClass("fa-caret-down");
+    $(this).children().toggleClass("fa-caret-up");
+
   });
 });
