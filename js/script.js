2210c8de946793c4f00e6c8ae29afad7c5a371b6
Maxence Raynal <mraynal2008@gmail.com>
2020-02-25 19:29:52 +0100

    clear it

--- js/script.js
+++ js/script.js
@@ -1,42 +1,43 @@
 var modal = document.getElementById("theModal");
-let title = document.querySelector(".FCTitle");
-let question = document.querySelector(".FCQuestion");
-let answer = document.querySelector(".FCAnswer");
-let note = document.querySelector(".FCNote");
+var title = document.querySelector(".FCTitle");
+var question = document.querySelector(".FCQuestion");
+var answer = document.querySelector(".FCAnswer");
+var note = document.querySelector(".FCNote");
 load();
+
 function load() {
   var requestURL = "cards.json";
   var request = new XMLHttpRequest();
   request.open("GET", requestURL);
   request.responseType = "json";
   request.send();
-  request.onload = function() {
+  request.onload = function () {
     var loed = request.response;
     LoadCard(loed);
   };
 
   function LoadCard(jsonObj) {
-    $(title).text(jsonObj[cardNumber]["title"]);
-    $(question).text(jsonObj[cardNumber]["question"]);
-    $(answer).text(jsonObj[cardNumber]["answer"]);
-    $(note).text(jsonObj[cardNumber]["Note"]);
+    $(title).text(jsonObj[cardNumber].title);
+    $(question).text(jsonObj[cardNumber].question);
+    $(answer).text(jsonObj[cardNumber].answer);
+    $(note).text(jsonObj[cardNumber].Note);
   }
 }
 
 var cardNumber = 0;
-$(document).ready(function() {
+$(document).ready(function () {
   $(".nb-here").text(cardNumber);
   $(".dropdown-btn")
     .next()
     .hide();
-  $(".dropdown-btn").click(function() {
+  $(".dropdown-btn").click(function () {
     $(this)
       .next()
       .toggle(500);
     $(this).toggleClass("arrow-down");
     $(this).toggleClass("arrow-up");
   });
-  $(".prev-btn").click(function() {
+  $(".prev-btn").click(function () {
     cardNumber--;
     if (cardNumber < 0) {
       cardNumber = 0;
@@ -44,16 +45,16 @@ $(document).ready(function() {
     $(".nb-here").text(cardNumber);
     LoadCard(cardNumber);
   });
-  $(".next-btn").click(function() {
+  $(".next-btn").click(function () {
     cardNumber++;
     $(".nb-here").text(cardNumber);
     LoadCard(cardNumber);
   });
-  $(".dropdown-toggle").click(function() {
+  $(".dropdown-toggle").click(function () {
     $(this).toggleClass("arrow-down");
     $(this).toggleClass("arrow-up");
   });
-  $(".menuicocontainer").click(function() {
+  $(".menuicocontainer").click(function () {
     $(this).toggleClass("changetoN");
   });
   /*
@@ -61,13 +62,13 @@ $(document).ready(function() {
     var newUrl;
     $(".themeLoc").attr("href", newUrl);
   });*/
-  $(".modalbtn").click(function() {
+  $(".modalbtn").click(function () {
     modal.style.display = "block";
   });
-  $(".close").click(function() {
+  $(".close").click(function () {
     modal.style.display = "none";
   });
-  window.onclick = function(event) {
+  window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
